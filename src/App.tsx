cedb6dd14fa57514b47bdb6bc69478cd8b3efa9a
coitpark <minhobag18@gmail.com>
2024-04-11 23:07:57 +0900

    feat: router setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import BoardPage from '@/pages/BoardPage'
+import LoginPage from '@/pages/LoginPage';
+import ViewPage from '@/pages/ViewPage';
+import EditPage from '@/pages/EditPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<BoardPage/>} />
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/view' element={<ViewPage/>} />
+        <Route path='/edit' element={<EditPage/>} />
+      </Routes>
     </div>
   );
 }
