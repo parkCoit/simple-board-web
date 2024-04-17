bfd1207480c1f54229121e38ce730e33f99a2083
coitpark <minhobag18@gmail.com>
2024-04-18 08:30:23 +0900

    feat : set secret

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,14 +1,15 @@
 
 import React from 'react';
 import { Button } from "@/components/ui/button"
-import { REST_API_KEY, REDIRECT_URI } from "@/secret.json"
+
+import secrets from "@/secret.json";
 
 
 export interface LoginPageProps {
 }
 
 const LoginPage = (props: LoginPageProps) => {
-    
+    const { REST_API_KEY, REDIRECT_URI } = secrets;
     const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
     const loginHandler = () => {
         alert(link)
