76da42345ab4f5fbcbdcef568921fb9db7b3e3fe
coitpark <minhobag18@gmail.com>
2024-04-19 11:33:44 +0900

    feat : delete console

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -6,20 +6,20 @@ import secrets from "@/secret.json";
 
 
 export interface LoginPageProps {
+
 }
 
 const LoginPage = (props: LoginPageProps) => {
     const { REST_API_KEY, REDIRECT_URI } = secrets;
     const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
     const loginHandler = () => {
-        alert(link)
         window.location.href = link;
     };
 
     return <div>
         <Button type='button' onClick={loginHandler}>
-                        카카오 로그인
-                    </Button>
+            카카오 로그인
+        </Button>
     </div>;
 };
 
