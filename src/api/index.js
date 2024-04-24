31b8e7188076377634458e88e9dc3d73e6747632
coitpark <minhobag18@gmail.com>
2024-04-24 10:19:24 +0900

    feat : console 제거

--- src/api/index.js
+++ src/api/index.js
@@ -7,14 +7,12 @@ export const kakaoLoginData = req =>  axios.post(`${server}/kakao/login`, req)
 export const addBoard = req => axios.post(`${server}/board/`, req)
 export const boardUpdate = req => axios.put(`${server}/board/`, req)
 export const boardData = req => {
-    console.log('req : ',req)
     return axios.get(`${server}/board/`, {
         params : req
     })
 }
 
 export const deleteBoard = req => {
-    console.log('req : ',req)
     return axios.delete(`${server}/board/`, {
         params : req
     })
