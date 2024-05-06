973f16b49ae3fe9bd6a9c970709e01bcf02edf6e
coitpark <minhobag18@gmail.com>
2024-05-07 07:56:21 +0900

    feat : TopNavigation Setting

---
+++ src/components/common/TopNavigation.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function TopNavigation() {
+    return(
+        <div className="flex w-full border-b-2 p-4">
+            <Button>로그인</Button>
+        </div>
+    )
+}
