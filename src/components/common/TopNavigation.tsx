56501db1b2d4088a6125e692d5d08124f99dff1c
coitpark <minhobag18@gmail.com>
2024-05-07 19:34:21 +0900

    feat : TopNavigation 수정

--- src/components/common/TopNavigation.tsx
+++ src/components/common/TopNavigation.tsx
@@ -1,10 +1,40 @@
+import { useState, useEffect } from 'react';
 import { Button } from "@/components/ui/button";
-
+import { useNavigate, useLocation } from "react-router-dom"
 
 export default function TopNavigation() {
+
+  const navigate = useNavigate()
+
+  const location = useLocation();
+
+  const [token, setToken] = useState(sessionStorage.getItem('token'));
+
+
+
+  useEffect(() => {
+    setToken(sessionStorage.getItem('token'));
+  }, [location]);
+
+  const handleLogin = () => {
+    navigate('/login');
+  };
+
+  const handleLogout = () => {
+    sessionStorage.removeItem('token');
+    setToken(null);
+    alert('로그아웃 완료')
+    navigate('/');
+  };
+
     return(
         <div className="flex w-full border-b-2 p-4">
-            <Button>로그인</Button>
+            {token ? (
+            <Button onClick={handleLogout}> 로그아웃 </Button>
+          ) : (
+            <Button onClick={handleLogin}> 로그인 </Button>
+          )
+          }
         </div>
     )
 }
