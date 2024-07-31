2af9117e2f8fb5677414392ccfa26e2311d2ea9d
coitpark <minhobag18@gmail.com>
2024-08-01 06:14:34 +0900

    feat : gcp, firebase hosting 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import BoardPage from "@/pages/BoardPage";
 import LoginPage from "@/pages/LoginPage";
 import ViewPage from "@/pages/ViewPage";
@@ -17,6 +23,7 @@ function App() {
   const showTopNav = !hideTopNavPaths.includes(location.pathname);
 
   return (
+    // eslint-disable-next-line no-undef
     <div className="App">
       {showTopNav && <TopNavigation />}
       <Routes>
